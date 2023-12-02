5f78ee544cda0c1e98a7cb4cef03aa3a1fcf01f7
Sarahzulian <147708263+Sarahzulian@users.noreply.github.com>
2023-12-02 18:27:17 +0100

    gray box

--- frontend/src/pages/Projects/NoteBot/NoteList.jsx
+++ frontend/src/pages/Projects/NoteBot/NoteList.jsx
@@ -1,12 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { Grid, Typography, Button } from "@mui/material";
-import { purple } from "@mui/material/colors";
+import { Grid, Typography, Button, Paper } from "@mui/material";
 
 export default function NoteList() {
   return (
-    <Grid container alignItems="center" justifyContent="space-between">
-      <Typography> NoteList </Typography>
-      <Button variant="contained"> ADD NOTE </Button>
-    </Grid>
+    <>
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="space-between"
+        spacing={2}
+      >
+        <Grid item>
+          <Typography variant="h6">NoteList</Typography>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" color="primary">
+            ADD NOTE
+          </Button>
+        </Grid>
+        <Grid item>
+          <Paper
+            elevation={3} // Einstellung für die Schattenstärke
+            style={{ padding: "20px", backgroundColor: "#767676" }} // Hintergrundfarbe
+          >
+            <Typography variant="body1">Das hier ist eine Notiz</Typography>
+            {/* Weitere Inhalte können hier hinzugefügt werden */}
+          </Paper>
+        </Grid>
+      </Grid>
+    </>
   );
 }
