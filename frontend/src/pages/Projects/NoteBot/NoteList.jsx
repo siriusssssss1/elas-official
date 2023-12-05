0a750b671398dae6b4cb604c69173a18bafbe4fe
victoriafrommann <116568075+victoriafrommann@users.noreply.github.com>
2023-12-05 14:28:27 +0100

    update homepage

--- frontend/src/pages/Projects/NoteBot/NoteList.jsx
+++ frontend/src/pages/Projects/NoteBot/NoteList.jsx
@@ -2,66 +2,86 @@ import React, { useState, useEffect } from "react";
 import { Grid, Typography, Button, Paper } from "@mui/material";
 //import { useHistory } from "react-router-dom";
 
-export default function NoteList() {
-  //const history = useHistory();
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Grid from "@mui/material/Unstable_Grid2";
+import { Container} from "@mui/material";
+import { Image } from "../../components/Image";
+import { Typography, Button } from "@mui/material";
 
-  //const handleBoxClick = () => {
-  // Hier kannst du die URL oder den Pfad zur neuen Seite angeben
-  //const newPath = "/neue-seite";
-  // Hier wird die Navigation zur neuen Seite ausgelöst
-  //history.push(newPath);
-  //};
+
+
+
+export const Home = () => {
+  const navigate = useNavigate();
+
+
+  const [redirect,doRedirect]=useState(false);
+
+
+
+useEffect(()=>{
+  if(redirect){
+    navigate("/")
+  }
+},[redirect])
+
+
+const click = () =>{
+  navigate("/login")
+}
+
+
 
   return (
-    <>
-      <Grid
-        container
-        alignItems="center"
-        justifyContent="space-between"
-        spacing={2}
-      >
-        <Grid item>
-          <Typography variant="h6">NoteList</Typography>
-        </Grid>
-        <Grid item>
-          <Button variant="contained" color="primary">
-            ADD NOTE
+    <Container  spacing={4} sx={{marginTop:5}}>
+      <Grid container spacing={4}  sx={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding:2,
+                  width: "100%"
+                }}>
+        <Grid   xs={12}
+          md={5}>
+          <Typography variant="h4" fontSize="40px" fontWeight={500} 
+          sx={{ color: "#228B8D", textAlign: "left"}}
+          gutterBottom>
+            The Note - Taking Platform You Really Want
+          </Typography>
+          <Typography variant="body1" fontSize="20px" fontWeight={500} 
+          sx={{color:"#E3782F" , marginBottom:"1em"}} paragraph>
+                Take and Organise your notes with AI assistance
+          </Typography>
+          <Button
+          onClick={click}
+              size="large"
+              variant="outlined"
+              sx={{
+                background: "#4C8FCC",
+                borderRadius: 4,
+                color: "#FFFFFF",
+                alignItems: "left",
+                paddingX: 3,
+                border: "0",
+                textTransform: "none",
+                "&:hover": {
+                  color: "#FFFFFF",
+                  background: "#5ba0e1",
+                  border: "0",
+                },
+              }}>
+            Get Started
           </Button>
         </Grid>
-        <Grid item>
-          <Paper
-            elevation={3} // Einstellung für die Schattenstärke
-            style={{
-              padding: "20px",
-              margin: "10px",
-              backgroundColor: "#D9D9D9",
-              height: "200px", // Höhe anpassen
-              width: "250px", // Breite anpassen}} // Hintergrundfarbe
-            }}
-          >
-            <Typography variant="body1" style={{ color: "white" }}>
-              Das hier ist eine Notiz
-            </Typography>
-            {/* Weitere Inhalte können hier hinzugefügt werden */}
-          </Paper>
-
-          <Paper
-            elevation={3} // Einstellung für die Schattenstärke
-            style={{
-              padding: "20px",
-              margin: "10px",
-              backgroundColor: "#D9D9D9",
-              height: "200px", // Höhe anpassen
-              width: "250px", // Breite anpassen}} // Hintergrundfarbe
-            }}
-          >
-            <Typography variant="body1" style={{ color: "white" }}>
-              Das hier ist eine Notiz
-            </Typography>
-            {/* Weitere Inhalte können hier hinzugefügt werden */}
-          </Paper>
+        <Grid item xs={12} sm={6}>
+        <Image
+            width="100%" height="auto"
+            src='./home.png'
+            alt="start button"
+          />
         </Grid>
       </Grid>
-    </>
+    </Container>
   );
-}
+};
