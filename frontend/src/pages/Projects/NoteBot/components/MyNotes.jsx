6e5b542e78bcd0fba71f498731d2a45b40bce553
victoriafrommann <116568075+victoriafrommann@users.noreply.github.com>
2024-01-03 15:20:22 +0100

    layout

--- frontend/src/pages/Projects/NoteBot/components/MyNotes.jsx
+++ frontend/src/pages/Projects/NoteBot/components/MyNotes.jsx
@@ -1,11 +1,60 @@
 import React, { useState, useEffect } from "react";
 import { Grid, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
 
 export default function MyNotes() {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
   return (
     <Grid container sx={{ maxWidth: 1500, width: "100%" }} spacing={2}>
       <Grid item>
         <Typography>My Notes</Typography>
+      </Grid>
+      <Grid item>
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              Word of the Day
+            </Typography>
+            <Typography variant="h5" component="h2">
+              be{bull}nev{bull}o{bull}lent
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              adjective
+            </Typography>
+            <Typography variant="body2" component="p">
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
       </Grid>
     </Grid>
   );
