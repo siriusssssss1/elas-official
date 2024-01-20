5a0c82c6927c4a9c467bd7a8bb94310113e23d68
victoriafrommann <116568075+victoriafrommann@users.noreply.github.com>
2024-01-20 17:43:20 +0100

    idk

--- frontend/src/pages/Projects/NoteBot/components/Notes/chooseLayout.jsx
+++ frontend/src/pages/Projects/NoteBot/components/Notes/chooseLayout.jsx
@@ -1,6 +1,6 @@
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Unstable_Grid2";
-import { LayoutItem } from "../Layouts/LayoutItem.js"; // file still missing 
+import { LayoutItem } from "../Layouts/LayoutItem"; // file still missing -> components/Layouts/LayouItem
 import { useTheme } from "@mui/material/styles";
 
 export function LayoutSelector({ onLayoutSelect }) {
