324919ecdf245f8f10617530dd1ed6687f2446a6
paulas16 <116520371+paulas16@users.noreply.github.com>
2024-01-21 21:29:01 +0100

    get data from backend - get drafts

--- frontend/src/pages/Projects/NoteBot/components/Drafts.jsx
+++ frontend/src/pages/Projects/NoteBot/components/Drafts.jsx
@@ -2,13 +2,57 @@ import React, { useState, useEffect } from "react";
 import { Grid, Typography } from "@mui/material";
 import NoteCard from "./NoteCard";
 
-import { getCards } from "../utils/api";
+import { getDrafts } from "../utils/api";
 
 export default function Drafts() {
+  const [cards, setCards] = useState({
+    message: "",
+    cards: [],
+  });
+
+  useEffect(() => {
+    async function getCardInfo() {
+      const cardsInfo = await getDrafts();
+
+      if (cardsInfo.cards !== undefined) {
+        setCards(cardsInfo);
+        console.log(cardsInfo);
+      } else {
+        setCards((prevState) => ({
+          ...prevState,
+          message: cardsInfo.message,
+        }));
+      }
+    }
+    getCardInfo();
+  }, []);
+
+
+  // return (
+  //   <Grid container sx={{ maxWidth: 1500, width: "100%" }} spacing={2}>
+  //     <Grid item>
+  //     <Typography variant="h5" style={{ color: "#A5A5A5" }}>Drafts</Typography>
+  //     </Grid>
+  //   </Grid>
+  // );
+
   return (
-    <Grid container sx={{ maxWidth: 1500, width: "100%" }} spacing={2}>
-      <Grid item>
-      <Typography variant="h5" style={{ color: "#A5A5A5" }}>Drafts</Typography>
+    <Grid container spacing={2} sx={{ maxWidth: 1500, width: "100%" }} > 
+      <Grid item xs={12}>
+        <Typography
+          variant="h5"
+          style={{ color: "#A5A5A5", marginBottom: "20px" }}
+        >
+          My Notes
+        </Typography>
+      </Grid>
+      <Grid item container spacing={2}>
+        {cards.cards.map((card) => (
+
+          
+          <NoteCard key={card.id} card={card} style={{ marginBottom: "20px" }}/>
+
+        ))}
       </Grid>
     </Grid>
   );
--- frontend/src/pages/Projects/NoteBot/utils/api.js
+++ frontend/src/pages/Projects/NoteBot/utils/api.js
@@ -27,23 +27,27 @@ export const getCards = async () => {
    const responseNr2 = await Backend.get(`/notebot/notes/all`);
    console.log(responseNr2.data)
    
-   const response = {
-    data: {
-      message: "Cards found!",
-      courses: [
-        {
-          title: "Mathe",
-          isFavorite: false,
-          rating: 2.1,
-        },
-        {
-          title: "GPT",
-          isFavorite: true,
-          rating: 4.8,
-        }
-      ]
-    }
-  } 
+  return {cards: responseNr2.data.notes}; 
+    const { //relevante Daten (message und cards) werden destrukturiert und in den Variablen message und cards gespeichert
+      data: { message, courses },
+    } = responseNr2;
+
+    return { message, courses };
+  } catch (err) {
+    console.log(err);
+    return {
+      message: "Server not connected",
+      courses: undefined
+    };
+  }
+};
+
+export const getDrafts = async () => {
+  try {
+
+   const responseNr2 = await Backend.get(`/notebot/drafts/users/a19d4fd7-2052-42e4-8ab2-56db09944363`);
+   console.log(responseNr2.data)
+   
   return {cards: responseNr2.data.notes}; 
     const { //relevante Daten (message und cards) werden destrukturiert und in den Variablen message und cards gespeichert
       data: { message, courses },
