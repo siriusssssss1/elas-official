111d86c71c0356ef72cba13371dc6a38067e22b3
victoriafrommann <116568075+victoriafrommann@users.noreply.github.com>
2024-01-29 18:18:07 +0100

    update editNote

--- frontend/src/pages/Projects/NoteBot/components/Notes/editNote.jsx
+++ frontend/src/pages/Projects/NoteBot/components/Notes/editNote.jsx
@@ -1,80 +1,72 @@
 import React from "react";
-import {
-  Box,
-  Paper,
-  IconButton,
-  Typography,
-  Button,
-  Grid,
-} from "@mui/material";
+import { Box, Paper, IconButton, Typography, Button } from "@mui/material";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import TextFieldsIcon from "@mui/icons-material/TextFields";
 import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
 import PlayCircleFilledIcon from "@mui/icons-material/PlayCircleFilled";
-import TextField from "@mui/material/TextField";
 
 function EditNote() {
   const handleAddSection = () => {
     console.log("Add Section Clicked");
   };
 
+  // Diese Funktion könnte für das Hinzufügen von Widgets verwendet werden
   const handleAddWidget = (widgetType) => {
     console.log("Add Widget:", widgetType);
   };
 
+//raw code
   return (
     <Box sx={{ padding: 2 }}>
       <Paper elevation={3} sx={{ padding: 2, marginBottom: 2 }}>
-        <Grid container spacing={2}>
-          {/* Texteditor Bereich */}
-          <Grid item xs={12} md={6}>
-            <Box
-              sx={{ border: "1px solid #ccc", minHeight: "150px", padding: 1 }}
-            >
-              <TextField fullWidth label="Edit This  Note" id="fullWidth" />
-            </Box>
-          </Grid>
-          {/* Widget Auswahl Bereich */}
-          <Grid item xs={12} md={6}>
-            {/* Hier können Sie die rechte Seite mit Inhalten füllen */}
-            <Box sx={{ height: "150px", padding: 1, border: "1px solid #ccc" }}>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column", // Stapelt die Elemente vertikal
-                  alignItems: "center", // Zentriert die Elemente horizontal
-                  justifyContent: "center", // Zentriert die Elemente vertikal, wenn es zusätzlichen Platz gibt
-                  marginTop: 2,
-                }}
-              >
-                <Typography variant="subtitle2" sx={{ marginBottom: 2 }}>
-                  Choose a widget
-                </Typography>
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "row", // Ordnet die Icons horizontal an
-                    alignItems: "center", // Zentriert die Icons vertikal
-                    justifyContent: "center", // Zentriert die Icons horizontal
-                  }}
-                >
-                  <IconButton onClick={() => handleAddWidget("text")}>
-                    <TextFieldsIcon />
-                  </IconButton>
-                  <IconButton onClick={() => handleAddWidget("pdf")}>
-                    <PictureAsPdfIcon />
-                  </IconButton>
-                  <IconButton onClick={() => handleAddWidget("video")}>
-                    <PlayCircleFilledIcon />
-                  </IconButton>
-                </Box>
-              </Box>
-
-              {/* Hier können Sie die Widgets hinzufügen */}
-            </Box>
-          </Grid>
-        </Grid>
+        {/* Texteditor Bereich */}
+        <Box sx={{ border: "1px solid #ccc", minHeight: "150px", padding: 1 }}>
+          {/* Hier könnten Sie einen echten Texteditor wie 'react-quill' integrieren */}
+          <Typography variant="body1">Texteditor</Typography>
+        </Box>
+        {/* Widget Auswahl Bereich */}
+        <Box sx={{ display: "flex", justifyContent: "flex-end", marginTop: 2 }}>
+          <IconButton onClick={() => handleAddWidget("text")} 
+          sx={{
+            color: "white",
+            bgcolor: "blue", // Blauer Hintergrund für das Text-Widget
+            "&:hover": {
+              bgcolor: "darkblue", // Dunkleres Blau beim Hover
+            },
+            borderRadius: "50%",
+          }}
+          >
+            <TextFieldsIcon />
+          </IconButton>
+          <IconButton onClick={() => handleAddWidget("pdf")}
+          sx={{
+            color: "white",
+            bgcolor: "orange", // Orangener Hintergrund für das PDF-Widget
+            "&:hover": {
+              bgcolor: "darkorange", // Dunkleres Orange beim Hover
+            },
+            borderRadius: "50%",
+            marginLeft: 1, // Fügt Abstand zwischen den Icons hinzu
+          }}
+          >
+            <PictureAsPdfIcon />
+          </IconButton>
+          <IconButton onClick={() => handleAddWidget("video")}
+          sx={{
+            color: "white",
+            bgcolor: "red", // Roter Hintergrund für das Video-Widget
+            "&:hover": {
+              bgcolor: "darkred", // Dunkleres Rot beim Hover
+            },
+            borderRadius: "50%",
+            marginLeft: 1, // Fügt Abstand zwischen den Icons hinzu
+          }}
+          >
+            <PlayCircleFilledIcon />
+          </IconButton>
+        </Box>
       </Paper>
+      
     </Box>
   );
 }
