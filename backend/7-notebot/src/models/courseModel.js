323c4291ec3d822e9afdc7e611abf7a7d2a60c6a
siriusssssss1 <148116013+siriusssssss1@users.noreply.github.com>
2024-02-22 14:14:18 +0100

    coureModel Documentation

--- backend/7-notebot/src/models/courseModel.js
+++ backend/7-notebot/src/models/courseModel.js
@@ -1,23 +1,27 @@
+// Import the mongoose module
 const mongoose = require('mongoose');
 
+// Define the schema for the course
 const courseSchema = new mongoose.Schema({
-
+  // Title of the course
   title: {
     type: String,
-    required: true,
+    required: true, // Title is required
   },
+  // Notes related to the course,
   notes: [{ 
-    type: mongoose.Types.ObjectId,
-    ref:'Note'
+    type: mongoose.Types.ObjectId, 
+    ref: 'Note' // Notes are referenced from the Note model
   }],
+  // User ID of the user who owns the course, a required field of type String
   user_id: {
     type: String,
-    required: true,
-    ref:'User'
+    required: true, 
+    ref: 'User' // User is referenced from the User model
   },
-
 });
 
+// Create the Course model using the defined schema
 const Course = mongoose.model('Course', courseSchema);
 
 module.exports = Course;
