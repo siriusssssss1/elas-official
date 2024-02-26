43ad7d8fb2e08990d8af690f613e5166760aa99e
victoriafrommann <116568075+victoriafrommann@users.noreply.github.com>
2024-02-26 14:11:43 +0100

    editNote_cleanCode

--- frontend/src/pages/Projects/NoteBot/components/Notes/editNote.jsx
+++ frontend/src/pages/Projects/NoteBot/components/Notes/editNote.jsx
@@ -4,21 +4,17 @@ import {
   Paper,
   IconButton,
   Typography,
-  Button,
   Grid,
 } from "@mui/material";
-import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import { useState } from "react";
 import TextFieldsIcon from "@mui/icons-material/TextFields";
 import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
 import PlayCircleFilledIcon from "@mui/icons-material/PlayCircleFilled";
 import TextField from "@mui/material/TextField";
-import { useState } from "react";
 import UploadFileIcon from "@mui/icons-material/UploadFile";
 
+
 function EditNote() {
-  const handleAddSection = () => {
-    console.log("Add Section Clicked");
-  };
 
   const [selectedWidgetLeft, setSelectedWidgetLeft] = useState(null);
   const [selectedWidgetRight, setSelectedWidgetRight] = useState(null);
@@ -31,10 +27,10 @@ function EditNote() {
     setSelectedWidgetRight(widgetType);
   };
 
+  // Individual widgets are declared
   const TextWidget = () => (
     <TextField fullWidth label="Your Text" variant="outlined" multiline />
   );
-
   const PdfWidget = () => (
     <Box
       sx={{
@@ -45,19 +41,17 @@ function EditNote() {
       }}
     >
      <div style={{ textAlign: "center" }}>
-  <UploadFileIcon sx={{ fontSize: 60, color: "#ED7D31" }} />
-  <div style={{ color: "#ED7D31" }}>
-    UPLOAD FILE
-  </div>
-</div>
-
-          </Box>
+      <UploadFileIcon sx={{ fontSize: 60, color: "#ED7D31" }} />
+      <div style={{ color: "#ED7D31" }}>
+         UPLOAD FILE
+      </div>
+    </div>
+    </Box>
   );
-
-
-  const VideoWidget = () => (
+   const VideoWidget = () => (
     <TextField fullWidth label="Video Link" variant="outlined" />
   );
+
 
   const WidgetArea = ({ onAddWidget, selectedWidget }) => (
     <Box sx={{ height: "150px", padding: 1, border: "1px solid #ccc" }}>
@@ -70,7 +64,6 @@ function EditNote() {
             justifyContent: "center",
           }}
         >
-          {/* Box-Container für den Text und die Icons */}
           <Box
             sx={{
               display: "flex",
@@ -150,7 +143,8 @@ function EditNote() {
     <Box sx={{ padding: 2 }}>
       <Paper elevation={3} sx={{ padding: 2, marginBottom: 2 }}>
         <Grid container spacing={2}>
-          {/* Linke Seite */}
+
+          {/* Left Side */}
           <Grid item xs={12} md={6}>
             <WidgetArea
               onAddWidget={handleAddWidgetLeft}
@@ -158,7 +152,7 @@ function EditNote() {
             />
           </Grid>
 
-          {/* Rechte Seite */}
+          {/* Right Side */}
           <Grid item xs={12} md={6}>
             <WidgetArea
               onAddWidget={handleAddWidgetRight}
@@ -172,100 +166,3 @@ function EditNote() {
 }
 
 export default EditNote;
-
-//   return (
-//     <Box sx={{ padding: 2 }}>
-//       <Paper elevation={3} sx={{ padding: 2, marginBottom: 2 }}>
-//         <Grid container spacing={2}>
-//           {/* Texteditor Bereich (Links)*/}
-//           <Grid item xs={12} md={6}>
-//             <Box
-//               sx={{ border: "1px solid #ccc", minHeight: "150px", padding: 1 }}>
-//               {/* {selectedWidget === "text" && <TextWidget />}
-//               {selectedWidget === "pdf" && <PdfWidget />}
-//               {selectedWidget === "video" && <VideoWidget />} */}
-//               <TextField fullWidth label="Edit This  Note" id="fullWidth" />
-//             </Box>
-//           </Grid>
-//           {/* Widget Auswahl Bereich (Rechts)- wird nur angezeigt, wenn kein Widget ausgewählt ist*/}
-//           <Grid item xs={12} md={6}>
-//             {/* Hier können Sie die rechte Seite mit Inhalten füllen */}
-//             <Box sx={{ height: "150px", padding: 1, border: "1px solid #ccc" }}>
-//             {!selectedWidget && (
-//               <Box
-//                 sx={{
-//                   display: "flex",
-//                   flexDirection: "column", // Stapelt die Elemente vertikal
-//                   alignItems: "center", // Zentriert die Elemente horizontal
-//                   justifyContent: "center", // Zentriert die Elemente vertikal, wenn es zusätzlichen Platz gibt
-//                   //marginTop: 2,
-//                 }}
-//               >
-//                 <Typography variant="subtitle2" sx={{ marginBottom: 2 }}>
-//                   Choose a widget
-//                 </Typography>
-//                 <Box
-//                   sx={{
-//                     display: "flex",
-//                     flexDirection: "row", // Ordnet die Icons horizontal an
-//                     alignItems: "center", // Zentriert die Icons vertikal
-//                     justifyContent: "center", // Zentriert die Icons horizontal
-//                   }}
-//                 >
-//                   <IconButton onClick={() => handleAddWidget("text")}
-//                   sx={{
-//                     color: "white",
-//                     bgcolor: "blue", // Blauer Hintergrund für das Text-Widget
-//                     "&:hover": {
-//                       bgcolor: "darkblue", // Dunkleres Blau beim Hover
-//                     },
-//                     borderRadius: "50%",
-//                   }}
-//                 >
-//                     <TextFieldsIcon />
-//                   </IconButton>
-//                   <IconButton onClick={() => handleAddWidget("pdf")}
-//                   sx={{
-//                     color: "white",
-//                     bgcolor: "orange", // Orangener Hintergrund für das PDF-Widget
-//                     "&:hover": {
-//                       bgcolor: "darkorange", // Dunkleres Orange beim Hover
-//                     },
-//                     borderRadius: "50%",
-//                     marginLeft: 1, // Fügt Abstand zwischen den Icons hinzu
-//                   }}
-//                   >
-//                     <PictureAsPdfIcon />
-//                   </IconButton>
-//                   <IconButton onClick={() => handleAddWidget("video")}
-//                   sx={{
-//                     color: "white",
-//                     bgcolor: "red", // Roter Hintergrund für das Video-Widget
-//                     "&:hover": {
-//                       bgcolor: "darkred", // Dunkleres Rot beim Hover
-//                     },
-//                     borderRadius: "50%",
-//                     marginLeft: 1, // Fügt Abstand zwischen den Icons hinzu
-//                   }}
-//                   >
-//                     <PlayCircleFilledIcon />
-//                   </IconButton>
-//                 </Box>
-//               </Box>
-//             )}
-//             {/* Anzeigen des ausgewählten Widgets */}
-//               {selectedWidget === "text" && <TextWidget />}
-//               {selectedWidget === "pdf" && <PdfWidget />}
-//               {selectedWidget === "video" && <VideoWidget />}
-
-//               {/* Hier können Sie die Widgets hinzufügen */}
-//             </Box>
-//           </Grid>
-
-//         </Grid>
-//       </Paper>
-//     </Box>
-//   );
-// }
-
-// export default EditNote;
