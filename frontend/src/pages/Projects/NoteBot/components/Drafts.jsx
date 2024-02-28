cc8d32b7444ff38ccf1e2bce417ed8d11b635f74
Sarahzulian <147708263+Sarahzulian@users.noreply.github.com>
2024-02-28 14:07:06 +0100

    commenting

--- frontend/src/pages/Projects/NoteBot/components/Drafts.jsx
+++ frontend/src/pages/Projects/NoteBot/components/Drafts.jsx
@@ -3,22 +3,26 @@ import { Grid, Typography } from "@mui/material";
 import DraftCard from "./DraftCard";
 
 import { getDrafts } from "../utils/api";
-import { deleteNoteFromServer } from '../utils/api.js';
+import { deleteNoteFromServer } from "../utils/api.js";
 
-
+// The Drafts component is responsible for displaying the list of draft notes
 export default function Drafts() {
+  // State to store the draft cards and any messages (e.g., errors)
   const [cards, setCards] = useState({
     message: "",
     cards: [],
   });
 
+  // useEffect hook to fetch the draft cards from the server when the component mounts
   useEffect(() => {
     async function getCardInfo() {
-      const cardsInfo = await getDrafts();
+      const cardsInfo = await getDrafts(); // Fetching draft cards from the server
 
+        // Updating the state with the fetched cards or an error message
       if (cardsInfo.cards !== undefined) {
         setCards(cardsInfo);
         console.log(cardsInfo);
+        // notifyUserOfError(error);
       } else {
         setCards((prevState) => ({
           ...prevState,
@@ -32,22 +36,21 @@ export default function Drafts() {
   const handleDeleteNote = async (noteId) => {
     try {
       await deleteNoteFromServer(noteId);
-
       // If the deletion on the server is successful, update the state
       const updatedData = cards.cards.filter((card) => card.id !== noteId);
       setCards((prevState) => ({
         ...prevState,
         cards: updatedData,
       }));
+      
     } catch (error) {
-      // Handle any errors that occur during the server operation
       console.error("Error deleting note:", error);
     }
   };
 
-
+  // Rendering the Drafts component
   return (
-    <Grid container spacing={2} sx={{ maxWidth: 1500, width: "100%" }} > 
+    <Grid container spacing={2} sx={{ maxWidth: 1500, width: "100%" }}>
       <Grid item xs={12}>
         <Typography
           variant="h5"
@@ -58,14 +61,14 @@ export default function Drafts() {
       </Grid>
       <Grid item container spacing={2}>
         {cards.cards.map((card) => (
-
-          
-          <DraftCard key={card.id} card={card} style={{ marginBottom: "20px" }} handleDeleteNote={handleDeleteNote}/>
-
+          <DraftCard
+            key={card.id}
+            card={card}
+            style={{ marginBottom: "20px" }}
+            handleDeleteNote={handleDeleteNote} // Passing the delete handler function to each DraftCard
+          />
         ))}
       </Grid>
     </Grid>
   );
 }
-
-
