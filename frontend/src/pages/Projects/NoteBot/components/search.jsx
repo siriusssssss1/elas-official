f9ad12a7f621235387c88b69728ce0a327dfdb3b
Sarahzulian <147708263+Sarahzulian@users.noreply.github.com>
2024-02-28 20:22:32 +0100

    search_cleanCode
    deleting code that did not get implemented

--- frontend/src/pages/Projects/NoteBot/components/search.jsx
+++ frontend/src/pages/Projects/NoteBot/components/search.jsx
@@ -1,11 +1,8 @@
-import React, { useState } from 'react';
-import Toolbar from '@mui/material/Toolbar';
-//import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
-//import Autocomplete from '@mui/material/Autocomplete';
-import TextField from '@mui/material/TextField';
-//import Divider from '@mui/material/Divider';
-import { getNotesByCourseAndNoteTitle } from "../utils/api"
+import React, { useState } from "react";
+import Toolbar from "@mui/material/Toolbar";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
+import { getNotesByCourseAndNoteTitle } from "../utils/api";
 import {
   Routes,
   Route,
@@ -14,11 +11,9 @@ import {
   useLocation,
 } from "react-router-dom";
 
-
-const SearchComponent = () => {
-  const [searchValue, setSearchValue] = useState('');
+const Search = () => {
+  const [searchValue, setSearchValue] = useState("");
   const navigate = useNavigate();
-
 
   const handleInputChange = (event) => {
     setSearchValue(event.target.value);
@@ -27,96 +22,72 @@ const SearchComponent = () => {
   const handleSearchClick = async () => {
     // Make sure searchValue is defined before making the API call
     if (searchValue) {
-      //const userId = 'a19d4fd7-2052-42e4-8ab2-56db09944363';  // Replace with the actual user ID
       try {
         const result = await getNotesByCourseAndNoteTitle(searchValue);
         console.log(result);
-        navigate('/projects/notebot/search',
-          {
+        navigate("/projects/notebot/search", {
           state: { searchResults: result.cards, keyword: searchValue },
         });
-        
       } catch (error) {
-        console.error('Error fetching notes:', error);
-        
+        console.error("Error fetching notes:", error);
       }
     } else {
-      console.error('Search value is undefined or empty.');
+      console.error("Search value is undefined or empty.");
     }
   };
 
   const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       handleSearchClick();
     }
   };
 
-
-
   return (
     <div>
-      <Toolbar style={{ marginLeft: '80px', width: '80%', backgroundColor: 'transparent', padding: '0' }}>
-        <div style={{ marginTop: '-30px', position: 'relative', borderRadius: '0px', backgroundColor: 'transparent', width: '80%', height: '120%' }}>
+      <Toolbar
+        style={{
+          marginLeft: "80px",
+          width: "80%",
+          backgroundColor: "transparent",
+          padding: "0",
+        }}
+      >
+        <div
+          style={{
+            marginTop: "-30px",
+            position: "relative",
+            borderRadius: "0px",
+            backgroundColor: "transparent",
+            width: "80%",
+            height: "120%",
+          }}
+        >
           <div
             style={{
-              padding: '8px',
-              height: '100%',
-              position: 'absolute',
-              display: 'flex',
-              alignItems: 'center',
+              padding: "8px",
+              height: "100%",
+              position: "absolute",
+              display: "flex",
+              alignItems: "center",
             }}
             onClick={handleSearchClick}
           >
             <SearchIcon style={{ color: "#ED7D31" }} />
           </div>
-          {/* <Autocomplete
-            freeSolo
-            options={latestSearches}
-            inputValue={searchValue}
-            onInputChange={handleInputChange}
-            renderInput={(params) => (
-              <InputBase
-                {...params}
-                placeholder="SEARCH…"
-                style={{ padding: '8px 48px', width: '100%' }}
-                inputProps={{ 'aria-label': 'search' }}
-                onKeyDown={(e) => {
-                  if (e.key === 'Enter') {
-                    handleSearchClick();
-                  }}
-                }
-              />
-            )}
-          /> */}
-           <TextField
-              placeholder="SEARCH…"
-              style={{ padding: '8px 48px', width: '100%', color: 'transparent' }}
-              inputProps={{ 'aria-label': 'search' }}
-              value={searchValue}
-              onChange={handleInputChange}
-              onKeyDown={handleKeyDown}
-              variant="standard"
-            />
+
+          <TextField
+            placeholder="SEARCH…"
+            style={{ padding: "8px 48px", width: "100%", color: "transparent" }}
+            inputProps={{ "aria-label": "search" }}
+            value={searchValue}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+            variant="standard"
+          />
         </div>
       </Toolbar>
-      {/* {showLatestSearches && (
-        <div style={{ padding: '8px', marginLeft: '80px' }}>
-          <h4>Latest Searches:</h4>
-          <ul>
-            {latestSearches.map((search, index) => (
-              <li key={index}>{search}</li>
-            ))}
-          </ul>
-        </div>
-      )} */}
-      {/* Your content goes here */}
-      {/* <ul>
-        {searchResults.map((result) => (
-          <li key={result._id}>{result.title}</li>
-        ))}
-      </ul> */}
     </div>
   );
 };
 
-export default SearchComponent;
+export default Search;
